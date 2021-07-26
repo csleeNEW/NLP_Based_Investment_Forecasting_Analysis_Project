f13587c7de88f7d38ee5e493b724d7a2f18ccbf2
minyoung2123 <86936906+minyoung2123@users.noreply.github.com>
2021-07-26 10:24:03 +0900

    Update 등락률_추가하기.py

--- 데이터 수집 및 전처리 코드/등락률_추가하기.py
+++ 데이터 수집 및 전처리 코드/등락률_추가하기.py
@@ -5,6 +5,7 @@ http://konex.krx.co.kr/contents/MKD/04/0402/04020100/MKD04020100T3.jsp
 
 import openpyxl as op
 
+last = input('엑셀의 가장 마지막 행의 숫자를 입력해주세요:')
 filepath = "data.xlsx"
 
 wb = op.load_workbook(filepath) #workbook 객체 생성 
@@ -15,7 +16,7 @@ for r in ws.rows: # ws.rows는 가장 마지막 행이 있는 data의 위치를 
     row_index = r[0].row #r[0]는 for문의 각 행값에 따른 열 위치를 의미한다. 숫자 0은 첫번째 열을 말한다.
     ws['K1'] = '등락률'
     ws['K'+str(row_index)] =  "=C"+str(row_index)+"/B"+str(row_index+1)+"*100" #등락률 구하는 공식
-    ws['K249'] = '0.0' #마지막 값은 계산이 안돼서 임의로 값을 집어 넣음
+    ws['K'+last] = '0.0' #마지막 값은 계산이 안돼서 임의로 값을 집어 넣음
     wb.save("data1.xlsx")
     
 ### data1.xlsx파일을 열어서 직접 저장을 누른 후 사용 가능하다.
