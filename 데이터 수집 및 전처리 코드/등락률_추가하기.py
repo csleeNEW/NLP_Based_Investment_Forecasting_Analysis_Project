83c6185ffe86d6b134820c728efbf7447588a57e
minyoung2123 <86936906+minyoung2123@users.noreply.github.com>
2021-07-26 10:15:49 +0900

    Add files via upload

---
+++ 데이터 수집 및 전처리 코드/등락률_추가하기.py
@@ -0,0 +1,16 @@
+import openpyxl as op
+
+filepath = "data.xlsx"
+
+wb = op.load_workbook(filepath) #workbook 객체 생성 
+
+ws = wb.active  #worksheet 객체 생성 
+
+for r in ws.rows: # ws.rows는 가장 마지막 행이 있는 data의 위치를 나타낸다.
+    row_index = r[0].row #r[0]는 for문의 각 행값에 따른 열 위치를 의미한다. 숫자 0은 첫번째 열을 말한다.
+    ws['K1'] = '등락률'
+    ws['K'+str(row_index)] =  "=C"+str(row_index)+"/B"+str(row_index+1)+"*100" #등락률 구하는 공식
+    ws['K249'] = '0.0' #마지막 값은 계산이 안돼서 임의로 값을 집어 넣음
+    wb.save("data1.xlsx")
+    
+### data1.xlsx파일을 열어서 직접 저장을 누른 후 사용 가능하다.
